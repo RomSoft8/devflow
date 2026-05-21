4bd9662a1a22cec895b40fea8f27d8541cbbc15d
Roman Morash <romsoft@mail.ru>
2026-05-22 00:20:52 +0200

    Add user entity and repository

---
+++ backend/src/main/java/com/romsoft36/backend/entity/User.java
@@ -0,0 +1,25 @@
+package com.romsoft36.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
---
+++ backend/src/main/java/com/romsoft36/backend/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.romsoft36.backend.repository;
+
+import com.romsoft36.backend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
